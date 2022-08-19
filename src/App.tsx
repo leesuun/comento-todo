983b94d41136dd97aee8f2095106daa8757d459a
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-08-19 15:04:32 +0900

    style: ToDo 기본 html 스타일 컴포넌트로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+const Header = styled.header``;
+const Title = styled.h2``;
+const Main = styled.main``;
+const Section = styled.section``;
+const Footer = styled.footer``;
+
 function App() {
   return (
-    <div>
-      <header>
-        <title>TO-do</title>
-      </header>
-      <main>
-        <section></section>
-        <section></section>
-      </main>
-      <footer></footer>
-    </div>
+    <Wrapper>
+      <Header>
+        <Title>TO-do</Title>
+      </Header>
+      <Main>
+        <Section></Section>
+        <Section></Section>
+      </Main>
+      <Footer></Footer>
+    </Wrapper>
   );
 }
 
