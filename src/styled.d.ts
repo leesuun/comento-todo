92c070d46ec2b562d96d50f92fd454687a6afac9
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-08-19 18:03:55 +0900

    style: theme.js, styled.d.ts 테마 속성 재정

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,9 +3,9 @@ import "styled-components";
 // and extend them!
 declare module "styled-components" {
   export interface DefaultTheme {
-    L_SectionBgColor: string;
-    R_SectionBgColor: string;
+    SectionColor_R: string;
+    SectionColor_L: string;
+    BasicColor: string;
     textColor: string;
-    CalenderTitleColor: string;
   }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
 export const DarkTheme: DefaultTheme = {
-  L_SectionBgColor: "#8588DD",
-  R_SectionBgColor: "#000",
+  SectionColor_R: "#545353",
+  SectionColor_L: "#000",
+  BasicColor: "#8588DD",
   textColor: "#000",
-  CalenderTitleColor: "#8588DD",
 };
 
 export const LightTheme: DefaultTheme = {
-  L_SectionBgColor: "#8588DD",
-  R_SectionBgColor: "#000",
+  SectionColor_R: "#FFF",
+  SectionColor_L: "#8588DD",
+  BasicColor: "#8588DD",
   textColor: "#000",
-  CalenderTitleColor: "#8588DD",
 };
